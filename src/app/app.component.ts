67dad8980fe9c6eb587f0b1a1e8264c2fab14249
Fabricio Oliveira <oliveira.fabricio.santos@gmail.com>
2021-09-03 03:20:07 -0300

    Define o componente do template app.component.html, que realizar a comunicação com o service e busca os dados para a tela, através da variavel "employees". O componente implementa OnInit, pois necessita dos dados fornecidos pelo método findAllEmployess() assim que a tela é carregada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
+
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+  
+  ngOnInit(): void {
+    this.findAllEmployess();
+  }
+
+  public findAllEmployess(): void {
+    this.employeeService.findAllEmployess().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
+  }
 }
