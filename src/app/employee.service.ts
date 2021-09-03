71d847b9967cd78d3de6b9c98bc5c0ce8b7c9edb
Fabricio Oliveira <oliveira.fabricio.santos@gmail.com>
2021-09-03 03:36:49 -0300

    Define a url base do servidor back-end.

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,22 +2,23 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http"
 import { Observable } from "rxjs";
 import { Employee } from "./employee";
+import { environment } from "src/environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class EmployeeService {
-    private apiServerUrl = '';
+    private apiServerUrl = environment.apiBaseUrl;
 
     constructor(private http: HttpClient) { }
 
-    /* Retorna um Observable do tipo lista de Employee, que é uma 
+    /* Retorna um Observable do tipo lista de Employee, que é uma
      * interface que representa o valor do back-end;
-     * Se o retorno fosse genérico, poderia colocar any no lugar 
+     * Se o retorno fosse genérico, poderia colocar any no lugar
      * da interface Employee.
-     * 
+     *
      * A mesma coisa serve para o http request, que nesse método
-     * é GET e está dizendo que o tipo de retorno no corpo é uma 
+     * é GET e está dizendo que o tipo de retorno no corpo é uma
      * lista de Employee.
      */
     public findAllEmployess(): Observable<Employee[]> {
